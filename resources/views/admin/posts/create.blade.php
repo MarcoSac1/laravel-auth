b12390adcf0eff90e1bfe9b6b4fdfc297b47fb0e
MarcoSac1 <marcosaccone8@gmail.com>
2024-07-30 18:08:07 +0200

    created create-or-edit

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,64 +1,21 @@
-@extends('layouts.admin')
+@extends('admin.layouts.create-or-edit')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-8 p-3">
-            {{-- @if ($errors->any())
-                <div class="col-8">
-                    <ul class="alert alert-danger">
-                        @foreach ($errors->all() as $error )
-                            <li >
-                                {{ $error }}
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif --}}
 
-            <form action="{{ route ('admin.posts.store')}}" method="POST">
-                @method('POST')
-                @csrf
+@section('page-title')
 
-                <div class="mb-3">
-                    <h1>Create new post</h1>
-                </div>
+    Create new post
 
-                <div class="mb-3 ">
-                    <label for="title">Title</label>
-                    <input type="text" name="title" id="title" class="form-control mb-2" value="{{ old('title', $post->title)}}">
-                    @error('title')
-                    <div class="alert alert-danger mb-3">
-                        {{ $message }}
-                    </div>
-                    @enderror
-                </div>
+@endsection
 
-                <div class="mb-3">
-                    <label for="image_url">Image Url</label>
-                    <input type="text" name="image_url" id="image_url" class="form-control mb-2" value="{{ old('image_url', $post->image_url)}}">
-                    @error('image_url')
-                    <div class="alert alert-danger mb-3">
-                        {{ $message }}
-                    </div>
-                    @enderror
-                </div>
+@section('form-action')
 
-                <div class="mb-3">
-                    <label for="content">Content</label>
-                    <textarea name="content" id="content" rows="20" cols="88" class="form-controll mb-2" >{{ old('content', $post->content)}}</textarea>
-                    @error('content')
-                    <div class="alert alert-danger mb-3">
-                        {{ $message }}
-                    </div>
-                    @enderror
-                </div>
+{{ route('admin.posts.store') }}
 
-                <input type="submit" value="Create new post" class="btn btn-primary btn-lg">
-                <input type="reset" value="Reset fields" class="btn btn-warning btn-lg">
+@endsection
 
-            </form>
-        </div>
-    </div>
-</div>
+
+@section('form-method')
+
+    @method('POST')
+
 @endsection
